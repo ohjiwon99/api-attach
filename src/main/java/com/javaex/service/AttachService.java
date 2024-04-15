e579824578c8d72823479581f9fea78b71dc1197
ojiwon99 <dhwldnjs0420@naver.com>
2024-04-15 14:37:26 +0900

    2

--- src/main/java/com/javaex/service/AttachService.java
+++ src/main/java/com/javaex/service/AttachService.java
@@ -25,6 +25,7 @@ public class AttachService {
 		if (osName.contains("linux")) {
 			System.out.println("리눅스");
 			// 파일저장디렉토리
+			
 			saveDir = "/app/upload"; // Linux 경로. username을 실제 사용자 이름으로 변경하세요.
 		} else {
 			System.out.println("윈도우");
